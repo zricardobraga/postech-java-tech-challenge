abfbf0829927d3040725b30e561dc4a3d41e2ec7
fbrcmmelo <fbrcmmelo@gmail.com>
2024-10-07 23:42:27 -0300

    feat: criado estrutura applicacao para o projeto usando DDD.

---
+++ src/test/java/br/com/postech/java/tech/challenge/cortistyle/CortistyleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.postech.java.tech.challenge.cortistyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CortistyleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
